35927d1d439e95943a4d9ed5cc56c798a22dac4d
auliazahwaa007 <auliazahwaa007@gmail.com>
2025-11-21 14:58:39 +0700

    perubahan 1

--- SearchNilai07.java
+++ SearchNilai07.java
@@ -1,17 +1,30 @@
+import java.util.Scanner;
+
 public class SearchNilai07 {
     public static void main(String[] args) {
-     int[] arrNilai = {80, 85, 78, 96, 90, 82,86};
-     int key = 90;
-     int hasil = 0;
-     
-     for (int i = 0; i < arrNilai.length; i++){
-        if (key == arrNilai[i]) {
-            hasil = i;
-            break;
+        Scanner sc = new Scanner(System.in);
+
+     System.out.print("Masukkan banyaknya nilai yang akan di input: ");
+        int n = sc.nextInt();
+
+        int[] nilai = new int[n];
+
+        for (int i = 0; i < n; i++){
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + ": ");
+            nilai[i] = sc.nextInt();
         }
-     }
-    System.out.println();
-    System.out.println("Nilai "+key+" ketemu di indeks ke-"+hasil);
-    System.out.println();
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
+
+        boolean found = false;
+
+        for (int i = 0; i < n; i++){
+            if (nilai[i] == key) {
+                System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (i+1));
+                found = true;
+                break;
+            }
+        }
     }
 }
